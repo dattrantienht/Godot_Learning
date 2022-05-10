8db7de794ba7c017338dc23e76f8904be79cce4d
Tran Tien Dat <74689952+dattrantienht@users.noreply.github.com>
2022-05-10 10:03:40 +0700

    .

--- source/Actors/Player.gd
+++ source/Actors/Player.gd
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
 
 
